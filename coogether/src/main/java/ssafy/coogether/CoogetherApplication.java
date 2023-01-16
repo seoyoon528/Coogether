d1ab7e3abbac0d87a6d0df18ea2fb36d44a87314
gusrnqq@naver.com <gusrnqq@naver.com>
2023-01-16 11:07:30 +0900

    feat: 프로젝트 생성

---
+++ coogether/src/main/java/ssafy/coogether/CoogetherApplication.java
@@ -0,0 +1,13 @@
+package ssafy.coogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoogetherApplication.class, args);
+	}
+
+}
