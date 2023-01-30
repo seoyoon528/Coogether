861587a51cd234eb9a52c78c0f5b70fac4869251
gusrnqq@naver.com <gusrnqq@naver.com>
2023-01-30 15:45:01 +0900

    feat: UserJoinListDto 생성

---
+++ backend/src/main/java/coogether/backend/dto/UserJoinListDto.java
@@ -0,0 +1,37 @@
+package coogether.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.querydsl.core.annotations.QueryProjection;
+import coogether.backend.domain.CookingRoom;
+import coogether.backend.domain.User;
+import coogether.backend.domain.UserJoinList;
+import coogether.backend.dto.simple.SimpleCookingRoomDto;
+import coogether.backend.dto.simple.SimpleUserDto;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+public class UserJoinListDto {
+
+//    private SimpleCookingRoomDto cookingRoom;
+//    private SimpleUserDto user;
+    private int userJoinListId;
+    private LocalDateTime userJoinRegTime;
+
+    ///////////////////////////
+    private String userNickname;
+    @QueryProjection
+    public UserJoinListDto(UserJoinList userJoinList) {
+//        this.cookingRoom = new SimpleCookingRoomDto(userJoinList.getCookingRoom());
+//        this.user = new SimpleUserDto(userJoinList.getUser());
+        this.userJoinListId = userJoinList.getUserJoinListId();
+        this.userJoinRegTime = userJoinList.getUserJoinRegTime();
+
+        this.userNickname = userJoinList.getUser().getUserNickname();
+    }
+}
