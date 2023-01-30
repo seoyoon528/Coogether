9866efc458583e88e00262f990f68df12829dee9
DoYenong <ghkdehdud96@naver.com>
2023-01-30 16:47:59 +0900

    feat : 대기방 => 방송방 이동하는 타이머 구현

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,49 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 
 function Room() {
+  let changeTimer;
+  let remainingTime;
   const [isStart, setIsStart] = useState(false);
   const params = useParams();
   const location = useLocation();
+
   const { roomName } = location.state.roomName;
   const { king } = location.state.king;
   const { thumbnail } = location.state.thumbnail;
   const { anounce } = location.state.anounce;
   const { recipe } = location.state.recipe;
   const { startTime } = location.state.startTime;
+  const { targetTime } = location.state.targetTime;
 
-  const calculateTime = startTime => {
+  const hour = startTime.getHours();
+  const minute = startTime.getMinutes();
+  const START = `${hour}시: ${minute}분`;
+
+  // console.log(targetTime);
+
+  const calculateTime = () => {
     const currentTime = new Date().getTime();
-    const sTime = new Date(startTime).getTime();
-
-    const remainingDuration = sTime - currentTime;
-
+    const remainingDuration = targetTime - currentTime;
     return remainingDuration;
   };
 
-  const startCheck = startTime => {
-    setIsStart(true);
+  const changeCheck = useCallback(() => {
+    remainingTime = calculateTime(targetTime);
 
-    const remainingTime = calculateTime(startTime);
-    setTimeout(startCheck, 3000);
-  };
+    changeTimer = setTimeout(changeCheck, remainingTime);
+
+    // ms 로 5분(300000) 아래면 값을 변경하게 변경
+    if (remainingTime < 0) {
+      setIsStart(true);
+      clearTimeout(changeTimer);
+    }
+  }, []);
+
   useEffect(() => {
-    startCheck(3000);
+    changeTimer = setTimeout(changeCheck, remainingTime);
   }, []);
   console.log(isStart);
-  return (
+
+  // 방송대기방 컴포넌트라고 가정
+  const test = (
     <div>
       <p>제목 : {roomName}</p>
       <p>{params.roomId} 번 요리방 입니다</p>
       <img src={thumbnail} alt="" />
-      <p>{startTime} 시작</p>
+      <p>{START} 시작</p>
       <p>요리대장 : {king}</p>
       <p>레시피 : {recipe}</p>
       {anounce && <p>공지사항 : {anounce}</p>}
+    </div>
+  );
 
-      {/* 요리 대기방, 요리 진행방 컴포넌트를 넣고 time 아웃으로 자동으로 넘어가게하기 */}
+  // 실제 방송방이라고 가정할 것
+  const test2 = (
+    <div>
+      <p>지금 방송중</p>
+    </div>
+  );
+  return (
+    <div>
+      {!isStart && test}
+      {isStart && test2}
     </div>
   );
 }
+//  {/* 요리 대기방, 요리 진행방 컴포넌트를 넣고 time 아웃으로 자동으로 넘어가게하기 */}
 
 export default Room;
