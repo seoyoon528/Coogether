82392f87a74d74374112f511f34c4845f67e254c
DoYenong <ghkdehdud96@naver.com>
2023-01-30 14:35:59 +0900

    feat:자동전환 테스트

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 
 function Room() {
+  const [isStart, setIsStart] = useState(false);
   const params = useParams();
   const location = useLocation();
   const { roomName } = location.state.roomName;
@@ -11,12 +12,31 @@ function Room() {
   const { recipe } = location.state.recipe;
   const { startTime } = location.state.startTime;
 
+  const calculateTime = startTime => {
+    const currentTime = new Date().getTime();
+    const sTime = new Date(startTime).getTime();
+
+    const remainingDuration = sTime - currentTime;
+
+    return remainingDuration;
+  };
+
+  const startCheck = startTime => {
+    setIsStart(true);
+
+    const remainingTime = calculateTime(startTime);
+    setTimeout(startCheck, 3000);
+  };
+  useEffect(() => {
+    startCheck(3000);
+  }, []);
+  console.log(isStart);
   return (
     <div>
+      <p>제목 : {roomName}</p>
       <p>{params.roomId} 번 요리방 입니다</p>
       <img src={thumbnail} alt="" />
       <p>{startTime} 시작</p>
-      <p>제목 : {roomName}</p>
       <p>요리대장 : {king}</p>
       <p>레시피 : {recipe}</p>
       {anounce && <p>공지사항 : {anounce}</p>}
