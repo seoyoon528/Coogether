7b8ab41e69d1f48904b087019f5b9ff5166e44b2
BANNA <ghkdehdud96@naver.com>
2023-01-31 23:32:19 +0900

    feat : 더미데이터 이미지추가

--- frontend/src/components/Wrapper/Box/RecipeBox/RecipeBoxItem .jsx
+++ frontend/src/components/Wrapper/Box/RecipeBox/RecipeBoxItem .jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { RecipeBoxItemStyle } from './RecipeBoxItemStyle ';
 
 function RecipeBoxItem({ recipe }) {
-  const { name, image, thumbnail } = recipe;
+  const { name, thumbnail } = recipe;
   return (
     // 현 위치에는 서치 레시피 박스가 들어가야 한다
     <RecipeBoxItemStyle>
