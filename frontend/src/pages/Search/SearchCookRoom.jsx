e07c91450abd0b85b591718fe6ebd640a1a04773
DoYenong <ghkdehdud96@naver.com>
2023-01-31 15:12:56 +0900

    feat : 검색관련 구현중

--- frontend/src/pages/Search/SearchCookRoom.jsx
+++ frontend/src/pages/Search/SearchCookRoom.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import StreamList from '../../components/Wrapper/Box/StreamBox/streamList';
+import SearchBox from '../../components/Wrapper/Box/SearchBox/SearchBox';
 
 // 테스트용
 import gim from '../../assets/img/김찌.jpg';
@@ -34,19 +35,34 @@ const DUMMY_ROOM = [
 ];
 
 function SearchCookRoom() {
+  const [enterdItme, setEnterdItme] = useState('');
+
+  const TEXT = <p>참여하고 싶은 요리방 이름을 입력하세요</p>;
+
+  const onSaveEnteredItem = item => {
+    setEnterdItme(item);
+  };
+
+  // 레시피 서치 페이지에도 추가해주기
+  // HTTP 요청 보내야 함
+  // 비동기 요청 보내기
+  // enterdItme 이 비어있으면 전체 (/room/list)
+  // enterdItme 값이 있으면 검색어 기반 (/room/search/{recipeName})
+
+  // useEffect(() => {
+  //   console.log(enterdItme);
+  // }, [enterdItme]);
+
   return (
-    <div>
-      <p>요리방 검색 페이지 입니다</p>
+    <>
       <h2>참여하고 싶은 요리방을 찾아보세요</h2>
       <h5>다양한 사람들과 함께 요리를 해보고 기록을 남겨보세요</h5>
-      <span>wrapper 의 서치박스로 변경예졍</span>
-      <input type="text" />
-      <ul>
-        <li>
-          <StreamList DUMMY_ROOM={DUMMY_ROOM} />
-        </li>
-      </ul>
-    </div>
+      <span>wrapper 의 서치박스로 변경중</span>
+      {/* 레시피 서치 페이지에도 추가해주기 */}
+      <SearchBox onSaveEnteredItem={onSaveEnteredItem} TEXT={TEXT} />
+      <br />
+      <StreamList DUMMY_ROOM={DUMMY_ROOM} />
+    </>
   );
 }
 
