b091c70e7e8e96037c0a380b37e0cb0c8b4b7c11
BANNA <ghkdehdud96@naver.com>
2023-02-01 00:28:46 +0900

    feat : 레시피 박스 관련 작업

--- frontend/src/components/Wrapper/Box/RecipeBox/RecipeBoxItem .jsx
+++ frontend/src/components/Wrapper/Box/RecipeBox/RecipeBoxItem .jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { RecipeBoxItemStyle } from './RecipeBoxItemStyle ';
 
 function RecipeBoxItem({ recipe }) {
   const { name, thumbnail } = recipe;
   return (
-    // 현 위치에는 서치 레시피 박스가 들어가야 한다
+    // 현 위치에는 SearchRecipeBox 가 들어가야 한다 => 쟤는 디자인만 해주는 애임
     <RecipeBoxItemStyle>
       <img src={thumbnail} alt={`${name} 이미지`} />
-      <p>{name}</p>
+      <Link to="/Main">
+        <h4>{name}</h4>
+      </Link>
+      <hr />
+      <div>레시피 내용</div>
     </RecipeBoxItemStyle>
   );
 }
