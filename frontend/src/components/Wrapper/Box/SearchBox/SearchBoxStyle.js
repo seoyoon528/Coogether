f9b81450e78d77d8d29932862ab2db25a720679d
BANNA <ghkdehdud96@naver.com>
2023-02-01 00:29:59 +0900

    feat : 서치박스 CSS 관련 작업

--- frontend/src/components/Wrapper/Box/SearchBox/SearchBoxStyle.js
+++ frontend/src/components/Wrapper/Box/SearchBox/SearchBoxStyle.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const SearchFormWrapper = styled.form`
   display: flex;
   justify-content: center;
-  height: 64px;
+  height: auto;
 
   & input {
     border-radius: 1rem;
-    height: 6.5rem;
-    width: 50rem;
+    height: 5rem;
+    width: 45rem;
     background-color: #ffffff;
     border: 0.2rem solid rgba(254, 189, 47, 0.5);
     font-size: 1.8rem;
