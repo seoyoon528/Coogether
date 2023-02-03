4e683e0b719eac5d14c51e54c97bc222bf51a62a
Deokho Kim <demiacan@gmail.com>
2023-02-03 15:10:47 +0900

    chore: eslint 수정

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'no-console': 0, // console 오류 제거 => 개발중일땐 오류 안뜨게 설정해두고 개발 끝나면 켜서 log 지우기
     'import/prefer-default-export': 'off', // export default 없으면 생기는 오류를 꺼줌
     'jsx-a11y/label-has-associated-control': 'off', // label 사용시 input을 label 안에 작성하도록 하는 기능 off
+    'react/jsx-props-no-spreading': 'off', // props spread 오류를 꺼둠
   },
 };
