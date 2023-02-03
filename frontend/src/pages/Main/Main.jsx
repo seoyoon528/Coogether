189d2eff8590e8b2bdb07f9e5548e3ff0f67c26f
DoYenong <ghkdehdud96@naver.com>
2023-02-03 17:48:17 +0900

    feat : 플로팅버튼 색 변경

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 
 import StreamList from '../../components/Wrapper/Box/StreamBox/streamList';
-import FloatBtn from '../../components/Btn/FloatBtn/FloatBtn';
 
 // 테스트용
 import gim from '../../assets/img/김찌.jpg';
@@ -47,7 +46,6 @@ function Main() {
         변경해야합니다]
       </p>
       {/* <StreamList DUMMY_ROOM={DUMMY_ROOM} /> */}
-      <FloatBtn />
     </div>
   );
 }
