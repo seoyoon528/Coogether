6f5bd80df5849cfeb1e656b388b0f3af36adea00
gusrnqq@naver.com <gusrnqq@naver.com>
2023-02-03 17:27:31 +0900

    feat: SwaggerConfig 수정

--- backend/src/main/java/coogether/backend/config/SwaggerConfig.java
+++ backend/src/main/java/coogether/backend/config/SwaggerConfig.java
@@ -1,19 +1,33 @@
 package coogether.backend.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private final TypeResolver typeResolver;
 
     private static final String API_NAME = "쿠게더 Project API";
     private static final String API_VERSION = "0.0.1";
@@ -22,6 +36,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .alternateTypeRules(AlternateTypeRules
+                        .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -36,5 +52,18 @@ public class SwaggerConfig {
                 .description(API_DESCRIPTION)
                 .contact(new Contact("Notion", "https://obsidian-caution-650.notion.site/413d4eca2d46491c83f0c4543bee066c", "gusrnas@gmail.com"))
                 .build();
+    }
+
+    @Getter @Setter
+    @ApiModel
+    static class Page {
+        @ApiModelProperty(value = "페이지 번호(0..N)")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
+        private Integer size;
+
+        @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
+        private List<String> sort;
     }
 }
