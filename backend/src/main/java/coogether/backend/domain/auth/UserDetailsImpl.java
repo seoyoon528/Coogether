84c90f95bc44531d52fa89fda87a32fa96ac12ec
seoyoon528 <seoyoon528@gmail.com>
2023-02-04 11:39:14 +0900

    feat: UserDetailsImpl 추가

---
+++ backend/src/main/java/coogether/backend/domain/auth/UserDetailsImpl.java
@@ -0,0 +1,60 @@
+package coogether.backend.domain.auth;
+
+import coogether.backend.domain.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private User user;
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getUserRoleType().toString();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
