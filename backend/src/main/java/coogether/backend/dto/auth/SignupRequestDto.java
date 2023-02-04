c09a115ea75aba855998e12380b0450b0d7252c9
seoyoon528 <seoyoon528@gmail.com>
2023-02-04 11:23:05 +0900

    feat: Signup, Login 관련 Dto

---
+++ backend/src/main/java/coogether/backend/dto/auth/SignupRequestDto.java
@@ -0,0 +1,27 @@
+package coogether.backend.dto.auth;
+
+import coogether.backend.domain.status.EnumUserCookCategory;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class SignupRequestDto {
+    String id;
+    String name;    // kakao에서 넘겨주는 nickname 값
+    String email;
+    String nickname;
+    String profileImg;
+    String userIntroduce;
+    EnumUserCookCategory userCookCategory;
+
+    @Builder
+    public SignupRequestDto(String id, String name, String email, String nickname, String profileImg, String userIntroduce, EnumUserCookCategory userCookCategory) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImg = profileImg;
+        this.userIntroduce = userIntroduce;
+        this.userCookCategory = userCookCategory;
+    }
+}
