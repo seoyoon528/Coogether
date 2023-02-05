08e8b0993b8e91b59d9382cf20e69683ea3c0294
Deokho Kim <demiacan@naver.com>
2023-02-05 19:53:46 +0900

    chore: profile page 스타일 적용

--- frontend/src/components/Wrapper/Box/Profile/ProfileSwiper.jsx
+++ frontend/src/components/Wrapper/Box/Profile/ProfileSwiper.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// MUI
+import { Stack } from '@mui/material';
+
 // Swiper
 import { Navigation, Mousewheel } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -11,8 +14,11 @@ import 'swiper/css/navigation';
 // eslint-disable-next-line
 import 'swiper/css/pagination';
 
+// Style
+import { HistoryStyle, MyRecipeStyle } from './ProfileSwiperStyle';
+
 export default function ProfileSwiper(props) {
-  const { histories } = props;
+  const { histories, recipes } = props;
   return (
     <Swiper
       modules={[Navigation, Mousewheel]}
@@ -20,29 +26,82 @@ export default function ProfileSwiper(props) {
       slidesPerView={3}
       navigation
       mousewheel
-      onSwiper={swiper => console.log(swiper)}
+      // onSwiper={swiper => console.log(swiper)}
       // onSlideChange={() => console.log('slide change')}
     >
-      {histories.map(history => {
-        return (
-          <SwiperSlide key={history.id}>
-            <div className="card">
-              <div className="card__image">
-                <img src={history.image} alt="요리 사진" />
-              </div>
-              <h4 className="card__title">{history.title}</h4>
-              <div className="card__content">
-                <p className="cook">
-                  {history.cooks.map(cook => {
-                    return <span key={cook}>{cook}</span>;
-                  })}
-                </p>
-                <p className="date">{history.date}</p>
-              </div>
-            </div>
-          </SwiperSlide>
-        );
-      })}
+      {histories &&
+        histories.map(history => {
+          return (
+            <SwiperSlide key={history.id}>
+              <HistoryStyle>
+                <div className="history__image">
+                  <img src={history.image} alt="요리 사진" />
+                </div>
+                <h4 className="history__title">{history.title}</h4>
+                <div className="history__content">
+                  <p className="cook">
+                    {history.cooks.map(cook => {
+                      return <span key={cook}>{cook}</span>;
+                    })}
+                  </p>
+                  <p className="date">{history.date}</p>
+                </div>
+              </HistoryStyle>
+            </SwiperSlide>
+          );
+        })}
+      {recipes &&
+        recipes.map(recipe => {
+          return (
+            <SwiperSlide key={recipe.id}>
+              <MyRecipeStyle>
+                <div className="my-recipe__image">
+                  <img src={recipe.image} alt="레시피 사진" />
+                </div>
+                <div className="my-recipe__text">
+                  <h4 className="my-recipe__title">{recipe.recipeName}</h4>
+                  <Stack
+                    spacing={2}
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                  >
+                    <div className="content__item">
+                      <div className="category">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/2940/2940816.png"
+                          alt="재료 아이콘"
+                        />
+                        <p>재료</p>
+                      </div>
+                      <p>N개</p>
+                    </div>
+                    <div className="content__item">
+                      <div className="category">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/3078/3078984.png"
+                          alt="단계 아이콘"
+                        />
+                        <p>과정</p>
+                      </div>
+                      <p>N개</p>
+                    </div>
+                    <div className="content__item">
+                      <div className="category">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/1627/1627309.png"
+                          alt="재료 아이콘"
+                        />
+                        <p>작성일</p>
+                      </div>
+                      <p>23.01.01</p>
+                    </div>
+                  </Stack>
+                </div>
+              </MyRecipeStyle>
+            </SwiperSlide>
+          );
+        })}
     </Swiper>
   );
 }
--- frontend/src/pages/User/Profile/Profile.jsx
+++ frontend/src/pages/User/Profile/Profile.jsx
@@ -7,8 +7,7 @@ import { Stack } from '@mui/material';
 import UserInfoBox from '../../../components/Wrapper/Box/Profile/UserInfoBox';
 import ProfileInformation from '../../../components/Wrapper/Box/Profile/ProfileInformation';
 import ProfileImage from '../../../components/Wrapper/Box/Profile/ProfileImage';
-import CookHistory from '../../../components/Wrapper/Box/Profile/CookHistory';
-import MyRecipe from '../../../components/Wrapper/Box/Profile/MyRecipe';
+import UserHistory from '../../../components/Wrapper/Box/Profile/UserHistory';
 
 // Style
 import { ProfileStyle } from './ProfileStyle';
@@ -23,6 +22,33 @@ const DUMMY_DATA = {
   like: '한식',
   rank: 'red',
   message: '상태 메시지입니다',
+  cookHistories: [
+    {
+      image:
+        'https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+      title: '레시피 제목',
+      cooks: [
+        '참여 요리사1',
+        '참여 요리사2',
+        '참여 요리사3',
+        '참여 요리사4',
+        '참여 요리사5',
+      ],
+      date: '2023-01-01',
+    },
+  ],
+  recipeHistories: [
+    {
+      image:
+        'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80',
+      recipeName: '요리 이름',
+      information: {
+        ingredients: [],
+        orders: [],
+        date: '',
+      },
+    },
+  ],
 };
 function Profile() {
   const {
@@ -34,8 +60,10 @@ function Profile() {
     like,
     rank,
     message,
+    cookHistories,
+    recipeHistories,
   } = DUMMY_DATA;
-  const profileInformation = {
+  const userInformation = {
     nickname,
     follower,
     following,
@@ -44,15 +72,34 @@ function Profile() {
     rank,
     message,
   };
+  const histories = [];
+  for (let i = 0; i < 8; i += 1) {
+    const data = { ...cookHistories[0] };
+    data.id = i;
+    histories.push(data);
+  }
+  const recipes = [];
+  for (let i = 0; i < 9; i += 1) {
+    const data = { ...recipeHistories[0] };
+    data.id = i;
+    recipes.push(data);
+  }
+
   return (
     <ProfileStyle>
       <Stack spacing={5} className="profile">
         <UserInfoBox className="user-information">
           <ProfileImage image={image} />
-          <ProfileInformation profileInformation={profileInformation} />
+          <ProfileInformation userInformation={userInformation} />
         </UserInfoBox>
-        <CookHistory />
-        <MyRecipe />
+        <hr />
+        {cookHistories.length > 0 && (
+          <UserHistory sectionName="요리 기록" histories={histories} />
+        )}
+        {recipeHistories.length > 0 && (
+          <UserHistory sectionName="등록한 레시피" recipes={recipes} />
+        )}
+        <UserHistory />
       </Stack>
     </ProfileStyle>
   );
