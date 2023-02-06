95c2b98150a45d1072f2e26045348a8954479643
이석훈 <cqe9933@naver.com>
2023-02-06 13:38:38 +0900

    chore: openVidu 적용 및 임시 로그인 로직 구현 시작

---
+++ frontend/src/pages/Room/RoomStyle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  position: relative;
+  background-size: cover;
+`;
