693106d0280567a943f8f92cf671220ffbe22d04
yeahzizi <newyeahzizi@gmail.com>
2023-02-07 16:04:07 +0900

    feat: 요리방 만들기 페이지 css 및 기본 기능 구현

--- frontend/src/components/Wrapper/Box/MakeCookRoomBox/MakeDetailInfo.jsx
+++ frontend/src/components/Wrapper/Box/MakeCookRoomBox/MakeDetailInfo.jsx
@@ -1,47 +1,18 @@
-import React, { useState, useRef } from 'react';
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import { SvgIcon } from '@mui/material';
-import { StreamContents, Input, Area } from './MakeDetailInfoStyle';
+import React from 'react';
+import { StreamContents } from './MakeDetailInfoStyle';
 
-function MakeBasicInfo() {
-  const [imgFile, setImgFile] = useState('');
-  const imgRef = useRef();
-  const saveImgFile = () => {
-    const file = imgRef.current.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImgFile(reader.result);
-    };
-  };
-  // const saveFileImage = e => {
-  //   setImgFile(URL.createObjectURL(e.target.files[0]));
-  // };
-
+function MakeBasicInfo(props) {
+  const { detailInputs, onChange: setDetailInputs } = props;
   return (
     <>
       <StreamContents>
         <p>공지 사항</p>
-        <Input type="text" placeholder="공지 사항을 입력해주세요" />
-        <p>미리 보기</p>
-        <label htmlFor="recipe-cook-image">
-          <Area>
-            {imgFile !== '' ? (
-              <img src={imgFile} alt="사진" />
-            ) : (
-              <SvgIcon
-                sx={{ fontSize: 100 }}
-                component={AddPhotoAlternateIcon}
-              />
-            )}
-          </Area>
-        </label>
         <input
-          type="file"
-          accept="image/*"
-          onChange={saveImgFile}
-          ref={imgRef}
-          hidden
+          type="text"
+          required
+          value={detailInputs}
+          onChange={e => setDetailInputs(e.target.value)}
+          placeholder="공지 사항을 입력해주세요"
         />
       </StreamContents>
     </>
