fdcc6c93e1fbf11ede2549a2cbbedb05e322a8b8
DoYenong <ghkdehdud96@naver.com>
2023-02-07 14:39:13 +0900

    feat : 요리 검색페이지, 레시피 페이지 그리드

--- frontend/src/components/Wrapper/Box/StreamBox/streamItemStyle.js
+++ frontend/src/components/Wrapper/Box/StreamBox/streamItemStyle.js
@@ -2,11 +2,9 @@ import styled from 'styled-components';
 
 export const CookRoomItemWrapper = styled.div`
   height: 25.5rem;
-  width: 21.6rem;
+  /* width: 21.6rem; */
   /* background-color: rgba(255, 248, 234, 0.5); */
   background-color: #fff8ea;
-
-  border-radius: 2rem;
 `;
 
 export const CookRoomItemImg = styled.img`
