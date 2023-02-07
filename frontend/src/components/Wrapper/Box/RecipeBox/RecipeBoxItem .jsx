f98e9011bd51342398537986e5d112725a7b0a99
DoYenong <ghkdehdud96@naver.com>
2023-02-07 09:26:32 +0900

    feat : 레시피 컨텐츠 말줄임

--- frontend/src/components/Wrapper/Box/RecipeBox/RecipeBoxItem .jsx
+++ frontend/src/components/Wrapper/Box/RecipeBox/RecipeBoxItem .jsx
@@ -6,10 +6,8 @@ import RecipeDetail from '../../../Modal/RecipeModal/RecipeDetail';
 // Style
 import { RecipeBoxItemStyle } from './RecipeBoxItemStyle ';
 
-import ggim from '../../../../assets/img/김찌.jpg';
-
 function RecipeBoxItem({ recipe }) {
-  const { recipeName, thumbnail, recipeContent, recipeId } = recipe;
+  const { recipeName, recipeImg, recipeContent, recipeId } = recipe;
   // Modal 상태
   const [isModalOpened, setIsModalOpened] = useState(false);
 
@@ -23,13 +21,12 @@ function RecipeBoxItem({ recipe }) {
 
   return (
     <RecipeBoxItemStyle>
-      {/* <img src={thumbnail} alt="이미지" /> */}
-      <img src={ggim} alt="이미지" />
+      <img src={recipeImg} alt="이미지" />
       <h4 onClick={openModal} aria-hidden="true">
         {recipeName}
       </h4>
       <hr />
-      <div>{recipeContent}</div>
+      <p>{recipeContent}</p>
       <RecipeDetail open={isModalOpened} onClose={closeModal} recipe={recipe} />
     </RecipeBoxItemStyle>
   );
