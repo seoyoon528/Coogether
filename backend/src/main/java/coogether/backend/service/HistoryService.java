900f7fdf1ffdb3684cda575cd52739e5ccdf471a
Leeh9 <40704078+Leeh9@users.noreply.github.com>
2023-02-09 00:42:18 +0900

    feat: 히스토리 썸네일 업로드 구현

--- backend/src/main/java/coogether/backend/service/HistoryService.java
+++ backend/src/main/java/coogether/backend/service/HistoryService.java
@@ -1,8 +1,13 @@
 package coogether.backend.service;
 
+import coogether.backend.domain.CookingRoom;
 import coogether.backend.domain.History;
+import coogether.backend.domain.Recipe;
 import coogether.backend.domain.User;
+import coogether.backend.domain.status.EnumCookingRoomStatus;
+import coogether.backend.repository.cookingroom.CookingRoomRepository;
 import coogether.backend.repository.history.HistoryRepository;
+import coogether.backend.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
@@ -16,8 +21,10 @@ import static coogether.backend.domain.QUser.user;
 @Transactional(readOnly = true)  // 트랜잭션 안에서만 데이터 변경하게 설정
 @RequiredArgsConstructor
 public class HistoryService {
-    private final HistoryRepository historyRepository;
 
+    private final HistoryRepository historyRepository;
+    private final CookingRoomRepository cookingRoomRepository;
+    private final UserRepository userRepository;
 
     public List<History> getHistoryByUserId(Long userSeq){
         return historyRepository.findByUserId(userSeq);
@@ -26,4 +33,23 @@ public class HistoryService {
         return historyRepository.findByCookingRoomId(userSeq, cookingRoomId);
     }
 
+    @Transactional
+    public History addHistory(Long userSeq, Long cookingRoomId, String url) {
+        // 기본 정보
+        History history = new History();
+
+        history.setHistoryImg(url);
+        CookingRoom cookingRoom = cookingRoomRepository.findByCookingRoomId(cookingRoomId);
+        // 요리방 상태 종료로 변경
+        cookingRoom.setCookingRoomStatus(EnumCookingRoomStatus.TERMINATED);
+        historyRepository.save(history);
+
+        User user = userRepository.findByUserSeq(userSeq);
+        history.setUser(user);
+        history.setCookingRoom(cookingRoom);
+
+        cookingRoomRepository.save(cookingRoom);
+
+        return history;
+    }
 }
