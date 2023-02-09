9aefb9552e2131feffb70eb129d4ad675fd90aa3
Deokho Kim <demiacan@gmail.com>
2023-02-09 10:53:07 +0900

    feat: 이미지 등록 확장자 제한

--- frontend/src/components/Recipe/Register/RecipeCookImage.jsx
+++ frontend/src/components/Recipe/Register/RecipeCookImage.jsx
@@ -33,7 +33,7 @@ export default function RecipeCookImage(props) {
             setCookImage(event.target.files[0]);
           }}
           type="file"
-          accept="image/*"
+          accept=".jpg, .jpeg, .png"
           id="recipe-cook-image"
           hidden
           name="recipeImg"
