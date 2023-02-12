984738b4e1e8f66b4ea692f42cc090cef372f852
yeahzizi <newyeahzizi@gmail.com>
2023-02-12 22:40:37 +0900

    chore: api 주소에 userseq 추가

--- frontend/src/pages/MyIngredientsManage/MyIngredientsManage.jsx
+++ frontend/src/pages/MyIngredientsManage/MyIngredientsManage.jsx
@@ -53,7 +53,8 @@ function MyIngredientsManage() {
   const favIngredient = i => {
     const inorOutIngredient = async target => {
       const sendIngredient = await axios.patch(
-        `https://i8b206.p.ssafy.io:9000/api/myIngredient/create/fav/1/${target}`,
+        // `https://i8b206.p.ssafy.io:9000/api/myIngredient/create/fav/1/${target}`,
+        `https://i8b206.p.ssafy.io:9000/api/myIngredient/create/fav/${isLogin}/${target}`,
         {}
       );
       console.log(sendIngredient.data);
@@ -67,7 +68,8 @@ function MyIngredientsManage() {
     const getData = async () => {
       try {
         const response = await axios.get(
-          `https://i8b206.p.ssafy.io:9000/api/myIngredient/list/fav/1`,
+          // `https://i8b206.p.ssafy.io:9000/api/myIngredient/list/fav/1`,
+          `https://i8b206.p.ssafy.io:9000/api/myIngredient/list/fav/${isLogin}`,
           {
             headers: {
               Authorization: `Bearer ${accessToken}`,
@@ -87,7 +89,8 @@ function MyIngredientsManage() {
   const sumbitIngredient = f => {
     const inorOutIngredient = async target => {
       const sendIngredient = await axios.patch(
-        `https://i8b206.p.ssafy.io:9000/api/myIngredient/update/1/${target}`,
+        // `https://i8b206.p.ssafy.io:9000/api/myIngredient/update/1/${target}`,
+        `https://i8b206.p.ssafy.io:9000/api/myIngredient/update/${isLogin}/${target}`,
         {}
       );
       console.log(sendIngredient);
@@ -101,7 +104,8 @@ function MyIngredientsManage() {
     const getData = async () => {
       try {
         const response = await axios.get(
-          `https://i8b206.p.ssafy.io:9000/api/myIngredient/list/total/1`
+          // `https://i8b206.p.ssafy.io:9000/api/myIngredient/list/total/1`
+          `https://i8b206.p.ssafy.io:9000/api/myIngredient/list/total/${isLogin}`
         );
         setFridge([...response.data.map((v, a) => v)]);
         console.log(fridge);
@@ -121,7 +125,8 @@ function MyIngredientsManage() {
           query = '';
         }
         const response = await axios.get(
-          `https://i8b206.p.ssafy.io:9000/api/ingredient/list/my/1/${query}`
+          // `https://i8b206.p.ssafy.io:9000/api/ingredient/list/my/1/${query}`
+          `https://i8b206.p.ssafy.io:9000/api/ingredient/list/my/${isLogin}/${query}`
         );
         setCategoryFridges([...response.data.map((v, a) => v)]);
       } catch (e) {
@@ -208,28 +213,28 @@ function MyIngredientsManage() {
 
       <Contents>
         <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}>
-          <Box gridColumn="span 1" />
-          <Box gridColumn="span 10">
+          {/* <Box gridColumn="span 1" /> */}
+          <Box gridColumn="span 12">
             <SearchIngredient
               searchIngre={searchIngre}
               favIngredient={favIngredient}
               sumbitIngredient={sumbitIngredient}
             />
           </Box>
-          <Box gridColumn="span 1" />
+          {/* <Box gridColumn="span 1" /> */}
         </Box>
         <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}>
-          <Box gridColumn="span 1" />
+          {/* <Box gridColumn="span 1" /> */}
           <Box gridColumn="span 3">
             <IngredientsBox category={category} onSelect={onSelect} />
           </Box>
           <Box gridColumn="span 1" />
-          <Box gridColumn="span 6">
+          <Box gridColumn="span 8">
             {components.map(component => {
               return component;
             })}
           </Box>
-          <Box gridColumn="span 1" />
+          {/* <Box gridColumn="span 1" /> */}
           {/* <Box gridColumn="span 1" /> */}
         </Box>
       </Contents>
