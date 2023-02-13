398380adbb890bdc7eac52428fbdd13aa8d28cd7
Deokho Kim <demiacan@gmail.com>
2023-02-13 16:00:57 +0900

    chore: console 출력 제거

--- frontend/src/components/Modal/RecipeModal/RecipeDetail.jsx
+++ frontend/src/components/Modal/RecipeModal/RecipeDetail.jsx
@@ -9,7 +9,6 @@ import RecipeContent from '../../Recipe/Modal/RecipeContent';
 export default function RecipeDetail(props) {
   // Props
   const { open, onClose, recipe } = props;
-  console.log(recipe);
 
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
