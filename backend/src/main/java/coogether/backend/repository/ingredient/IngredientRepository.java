2aee7b73b9aba468b722de2c3b6fc777e447cb8a
gusrnqq@naver.com <gusrnqq@naver.com>
2023-02-15 09:12:01 +0900

    feat : 카테고리별 내가 보유중인 재료 목록 api 수정

--- backend/src/main/java/coogether/backend/repository/ingredient/IngredientRepository.java
+++ backend/src/main/java/coogether/backend/repository/ingredient/IngredientRepository.java
@@ -12,13 +12,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Ingredient findByIngredientId(@Param("ingredientId")Long ingredientId);
+    Ingredient findByIngredientId(@Param("ingredientId") Long ingredientId);
 
     @Query("select i from Ingredient i where i.ingredientCategory = :categoryId")
     List<Ingredient> findByCategoryId(@Param("categoryId") EnumIngredientCategory categoryId);
+
     @Query("select i from Ingredient i join MyIngredientManage mm on i.ingredientId = mm.ingredient.ingredientId " +
-            "where mm.user.userSeq = :userSeq and i.ingredientCategory = :categoryId")
+            "where mm.user.userSeq = :userSeq and i.ingredientCategory = :categoryId and mm.myIngredientManageFlag = 'IN' ")
     List<Ingredient> findByUserSeqAndCategoryId(@Param("userSeq") Long userSeq, @Param("categoryId") EnumIngredientCategory categoryId);
+
     @Query("select i from Ingredient i where i.ingredientName like %:ingredientName% ")
     List<Ingredient> findByIngredientName(String ingredientName);
 }
