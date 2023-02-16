21164f737245968df514f3d60959e67d0a2f02b1
이석훈 <cqe9933@naver.com>
2023-02-16 09:19:19 +0900

    chore: redirect파일을 signin으로 변경

--- frontend/src/pages/User/Login/Login.jsx
+++ frontend/src/pages/User/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import * as L from './LoginStyle';
 
